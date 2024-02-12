2319c66a39bd745714af7640b6da4b031baf4617
guneyberkay <gnybky@gmail.com>
2024-02-12 21:08:39 +0300

    Update

--- Ball.js
+++ Ball.js
@@ -35,7 +35,7 @@ export default class Ball {
     }
     this.velocity = INITIAL_VELOCITY;
   }
-  update(delta) {
+  update(delta , paddleRects) {
     console.log("CALISIYOR");
     this.x += this.direction.x * this.velocity * delta;
     this.y += this.direction.y * this.velocity * delta;
@@ -45,12 +45,20 @@ export default class Ball {
     if (rect.bottom >= window.innerHeight || rect.top <= 0) {
       this.direction.y *= -1;
     }
-    if (rect.right >= window.innerWidth || rect.left <= 0) {
-      this.direction.x *= -1;
+    if (paddleRects.some(r => isCollision(r, rect))) {
     }
   }
 }
 
 function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min;
+}
+
+function isCollision(rect1, rect2) {
+  return(
+  rect1.left <= rect2.right &&
+  rect1.right >= rect2.left &&
+  rect1.top <=rect2.bottom &&
+  rect1.bottom >= rect2.top
+) 
 }
